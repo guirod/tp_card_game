e2b37c8f77d9a463c63127a63fe4d6b86081284d
guirod <roadyrod@gmail.com>
2023-11-16 12:29:17 +0100

    Add composer.json

--- deal.php
+++ deal.php
@@ -1,32 +1,38 @@
-<?php 
+<?php
 require_once('Deck.php');
 require_once('Hand.php');
 
-if (isset($_POST['nb_joueurs']) &&  isset($_POST['nb_cards']) && is_numeric($_POST['nb_joueurs']) && is_numeric($_POST['nb_cards'])) {
+if (isset($_POST['nb_joueurs']) && isset($_POST['nb_cards']) && is_numeric($_POST['nb_joueurs']) && is_numeric($_POST['nb_cards'])) {
     $deck = (new Deck())->initialize();
 
     $nbJoueurs = $_POST['nb_joueurs'];
     $nbCards = $_POST['nb_cards'];
-    $joueurs = [];
 
-    for($j=0; $j <$nbJoueurs; $j++) {
-        $joueurs[$j] = new Hand();
-    }
+    if ($nbJoueurs * $nbCards <= count($deck->getCards())) {
 
-    for ($i=0; $i<$nbCards; $i++) {
-        for($j=0; $j <$nbJoueurs; $j++) {
-            $joueurs[$j] = $joueurs[$j]->addCard($deck->drawCard());
+        $joueurs = [];
+
+        for ($j = 0; $j < $nbJoueurs; $j++) {
+            $joueurs[$j] = new Hand();
         }
-    }
 
-    foreach($joueurs as $key => $joueur) {
-        $joueur->sort();
-        
-        echo "<h1>Joueur$key</h1>";
-        foreach ($joueur->getCards() as $card) {
-            echo '<img src="' . $card->getUrl() . '" width="100px" />';
+        for ($i = 0; $i < $nbCards; $i++) {
+            for ($j = 0; $j < $nbJoueurs; $j++) {
+                $joueurs[$j] = $joueurs[$j]->addCard($deck->drawCard());
+            }
         }
-    }
 
-    echo Hand::getSize();
+        foreach ($joueurs as $key => $joueur) {
+            $joueur->sort();
+
+            echo "<h1>Joueur$key</h1>";
+            foreach ($joueur->getCards() as $card) {
+                echo '<img src="' . $card->getUrl() . '" width="100px" />';
+            }
+        }
+
+        echo Hand::getSize();
+    } else {
+        echo "STOP !";
+    }
 }
